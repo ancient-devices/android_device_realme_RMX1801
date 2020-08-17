93f72f9f921bf3511479f0f4236baf4545e2314c
Bruno Martins <bgcngm@gmail.com>
2020-08-17 10:56:33 +0700

    RMX1801: Move chargeonlymode
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,5 +1,16 @@
 LOCAL_PATH := $(call my-dir)
 
+# Offmode charging
+include $(CLEAR_VARS)
+LOCAL_MODULE          := chargeonlymode
+LOCAL_MODULE_OWNER    := realme
+LOCAL_MODULE_PATH     := $(TARGET_ROOT_OUT_SBIN)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
+LOCAL_SRC_FILES       := sbin/chargeonlymode
+LOCAL_MODULE_TAGS     := optional
+LOCAL_MODULE_CLASS    := EXECUTABLES
+include $(BUILD_PREBUILT)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
