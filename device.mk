c4b74477920df6d1ff3c4c7a8359ed1b452d4b7b
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2021-11-15 20:35:55 +0700

    [SQUASHED] RMX1801: Drop Pixel Powerhint
    * I'm lazy to pick eas stuff in kernel source
    
    Revert "RMX1801: rootdir: Set up/down_rate_limit_us as 1000"
    
    This reverts commit 3c0e291bfe6b659df2e2c444bc1bbaded78a7c88.
    
    Revert "RMX1801: power-libperfmgr: add a NOT_INTERACTIVE hint for setInteractive"
    
    This reverts commit ebd106509b6c62841c517212b85ee31fde2402a5.
    
    Revert "RMX1801: power-libperfmgr: add an updateHint function for simple hint ops"
    
    This reverts commit 7ac9da8397991698fa53b4501d160cccd714b5b6.
    
    Revert "RMX1801: libperfmgr.rc: let perfmgr can change dex2oat priorities"
    
    This reverts commit f8767a8180257268fab8dae0e9d955e52fa14d26.
    
    Revert "RMX1801: configs: Add AUDIO_LAUNCH to powerhint"
    
    This reverts commit 2bd1f237cda61d6982ed3db3de9e7a5198e78d9f.
    
    Revert "RMX1801: sepolicy: Address More Denials"
    
    This reverts commit d98c949f6799996617b2dd0c9fe9faeb546af70b.
    
    Revert "RMX1801: Add powerhint.json for sdm660"
    
    This reverts commit 532a84b95faef5684f68c4228dbf688da76c7739.
    
    Revert "RMX1801: Remove TARGET_USES_INTERACTION_BOOST flag"
    
    This reverts commit 17450b1a4d4237cad75aa6664435fcc43bd2bf00.
    
    Revert "RMX1801: rootdir: Switch to wahoo cpuset setup"
    
    This reverts commit ee8da04b3bdd699b0855fc07d666481d4e513882.
    
    Revert "RMX1801: Add init changes required for EAS & power-libperfmgr"
    
    This reverts commit 05fd2fcd98f3ec47729925dc5484723e733b1a8b.
    
    Revert "RMX1801: rootdir: remove interaction lock when idle"
    
    This reverts commit 1f28c6f223a06121ec875fcb8f9e4bbd61327a9e.
    
    Revert "RMX1801: Add sepolicy rules for power-libperfmgr"
    
    This reverts commit 8d1dfdba45b2bc41a295167a32b9b78a286a2735.
    
    Revert "RMX1801: Enable power-libperfmgr"
    
    This reverts commit df3882e3afef4e114279b4f580488cc501c2040f.
    
    Revert "RMX1801: power-libperfmgr: Add back powerstats"
    
    This reverts commit 4e6ee0c8f718a729cdbaf9877eb5b65515569cb2.
    
    Revert "RMX1801: power-libperfmgr: Remove ATRACE instances"
    
    This reverts commit 2fc6957d0f4fbb86ea711cd439cca8c181ff8a9d.
    
    Revert "RMX1801: power-libperfmgr: Follow symlinks when writing to dt2w toggle path"
    
    This reverts commit ed6508998b7d2ce3bfcb835d848b217855babb30.
    
    Revert "RMX1801: power-libperfmgr: Add support for tap-to-wake feature control"
    
    This reverts commit 333a0df60bb2bd4ab1486e6071bd4daf8ed707e1.
    
    Revert "RMX1801: power-libperfmgr: initialize powerHAL when boot is completed"
    
    This reverts commit 96a145a06eab78b8dda1f9e5214075c2b47c66b2.
    
    Revert "RMX1801: power-libperfmgr: Remove dumpstate support"
    
    This reverts commit 3c1429944040eb48a8a2650dabff325b281dcd6a.
    
    Revert "RMX1801: power-libperfmgr: completely remove camera hints handling"
    
    This reverts commit ddde9204d2c1c81278c4f696c85bc705e5da5b0f.
    
    Revert "RMX1801: power-libperfmgr: remove VR hints handling"
    
    This reverts commit c500ea49421f5484ba2054b81c57b91e9d6f7c5a.
    
    Revert "RMX1801: power-libperfmgr: remove Google-specific camera and audio hints"
    
    This reverts commit c41fe89a0622ed40503994b85f3d64be87ef1113.
    
    Revert "RMX1801: power-libperfmgr: remove Google-specific display LPM control"
    
    This reverts commit c042ef1ade2d808ac89618a0e46215ea4640bce4.
    
    Revert "RMX1801: power-libperfmgr: simply wait if idle state node is not present"
    
    This reverts commit 473cea9a23a0d500ac907512ded8132d1b5c7eee.
    
    Revert "RMX1801: power-libperfmgr: Adapt and rebrand for RMX1801"
    
    This reverts commit 1a7095f5e849405d5d838ca8d47baa94ec64fae9.
    
    Revert "RMX1801: Import Pixel libperfmgr Power HAL"
    
    This reverts commit 30886352c4d0eccc7f6fd54b8f35780cd8b2e9d9.
    
    Revert "RMX1801: Remove energy awareness daemon"
    
    This reverts commit c8f5bbd68725c56e51b2c11ad62754a0252a2fd5.
    
    Revert "RMX1801: Switch to taimen perfd blobs"
    
    This reverts commit 562648f43ebdf9b8f1e125baa6853c9a343a0b47.
    
    Revert "RMX1801: remove qti perf stack"
    
    This reverts commit a18b7a74cbb780adf684a196af6c4a88af4e166c.

--- device.mk
+++ device.mk
@@ -338,12 +338,12 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.0.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
     $(LOCAL_PATH)/configs/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.RMX1801-libperfmgr \
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock \
     vendor.qti.hardware.perf@2.2 \
     vendor.qti.hardware.perf@2.2.vendor
 
