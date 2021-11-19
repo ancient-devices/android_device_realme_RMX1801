168301f64a3299a8c983cda45eb028738bf41ed0
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2021-11-19 07:26:08 +0700

    RMX1801: init: Set device model, build fingerprint and description via init
    * Refs
    https://github.com/dev-harsh1998/android_device_realme_X2/blob/lineage-18.1/init/x2.cpp
    
    Co-authored-by: Harshit Jain <god@hyper-labs.tech>
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- init/rmx1801.cpp
+++ init/rmx1801.cpp
@@ -18,6 +18,24 @@
 
 #include "vendor_init.h"
 
+struct RMX180X_props
+{
+    std::string build_description;
+    std::string build_fingerprint;
+    std::string device_build;
+    std::string product_device;
+};
+
+std::vector<std::string> ro_props_default_source_order = {
+    "",
+    "bootimage",
+    "odm.",
+    "product.",
+    "system.",
+    "system_ext.",
+    "vendor.",
+};
+
 void property_override(char const prop[], char const value[], bool add = true)
 {
     prop_info *pi;
@@ -26,6 +44,59 @@ void property_override(char const prop[], char const value[], bool add = true)
         __system_property_update(pi, value, strlen(value));
     else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void setRMX(unsigned int variant)
+{
+    RMX180X_props prop[3] = {};
+
+    std::string build_desc = "sdm660_64-user 10 QKQ1.191014.001 eng.root.20200727.144331 release-keys";
+    std::string build_fingerprint = "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys";
+
+    // RMX1801
+    prop[0] = {
+        build_desc,
+        build_fingerprint,
+        "RMX1801",
+        "RMX1801",
+    };
+
+    // RMX1803
+    prop[1] = {
+        build_desc,
+        build_fingerprint,
+        "RMX1803",
+        "RMX1803",
+    };
+
+    prop[2] = {
+        build_desc,
+        build_fingerprint,
+        "RMX1807",
+        "RMX1807",
+    };
+
+    const auto set_ro_build_prop = [](const std::string &source,
+                                      const std::string &prop, const std::string &value) {
+        auto prop_name = "ro." + source + "build." + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    const auto set_ro_product_prop = [](const std::string &source,
+                                        const std::string &prop, const std::string &value) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    property_override("ro.build.description", prop[variant].build_description.c_str());
+    property_override("ro.build.fingerprint", prop[variant].build_fingerprint.c_str());
+    property_override("ro.build.product", prop[variant].product_device.c_str());
+    for (const auto &source : ro_props_default_source_order)
+    {
+        set_ro_build_prop(source, "fingerprint", prop[variant].build_fingerprint.c_str());
+        set_ro_product_prop(source, "device", prop[variant].product_device.c_str());
+        set_ro_product_prop(source, "model", prop[variant].device_build.c_str());
+    }
 }
 
 void set_dalvik()
@@ -45,18 +116,21 @@ void set_dalvik()
         heapgrowthlimit = "256m";
         heaptargetutilization = "0.46";
         heapmaxfree = "48m";
+        setRMX(2);
     } else if (sys.totalram > 4096ull * 1024 * 1024) {
         // from - phone-xhdpi-6144-dalvik-heap.mk
         heapstartsize = "16m";
         heapgrowthlimit = "256m";
         heaptargetutilization = "0.5";
         heapmaxfree = "32m";
+        setRMX(1);
     } else {
         // from - phone-xhdpi-4096-dalvik-heap.mk
         heapstartsize = "8m";
         heapgrowthlimit = "192m";
         heaptargetutilization = "0.6";
         heapmaxfree = "16m";
+        setRMX(0);
 	}
 
     property_override("dalvik.vm.heapstartsize", heapstartsize);
